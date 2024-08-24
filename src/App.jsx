d826b04b83100acb1abee564237ae4d63c2b1078
Yogesh Aithani <aithaniyogesh007@gmail.com>
2024-08-24 17:02:01 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <p>Hello how are you!</p>;
+}
+
+export default App;
